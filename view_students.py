82c5a7455fd4a76daa095fef2f692d92fd0d0dff
Alex <educateale12@gmail.com>
2026-02-15 12:53:29 +0300

    Add files via upload
    The view_students function is designed to retrieve and display all student records stored in the system. It helps users quickly check who is registered and view their details in a structured format.

---
+++ view_students.py
@@ -0,0 +1,7 @@
+def view_students(students):
+    if not students:
+        print("No students found.")
+    else:
+        for i, student in enumerate(students, start=1):
+            name, age, marks = student
+            print(f"{i}. Name: {name}, Age: {age}, Marks: {marks}")
