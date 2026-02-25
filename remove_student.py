bd9c0260223b184e307967f18870d36c063b6f1d
gtrevor025-a11y <gtrevor025@gmail.com>
2026-02-25 12:04:39 +0300

    Add function to remove a student from the list

--- remove_student.py
+++ remove_student.py
@@ -1,1 +1,14 @@
 
+def remove_student(students):
+    # Ask for input and clean up the formatting
+    raw_name = input("What is the students name to remove? ").strip()
+    rem_student = " ".join(raw_name.split()).title()
+    
+    # Loop through the list to find the match
+    for student in students:
+        if student["name"] == rem_student:
+            students.remove(student)
+            print("Student removed successfully!")
+            return 
+            
+    print("Student not found")
